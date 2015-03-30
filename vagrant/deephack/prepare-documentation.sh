776bf4b0e34d9b30757e784c5a9400826f9641e0
Craig Munro <craig.munro@gmail.com>
2015-03-30 12:01:37 +0100

    set firefox homepage

--- vagrant/deephack/prepare-documentation.sh
+++ vagrant/deephack/prepare-documentation.sh
@@ -11,3 +11,5 @@ find . -type f -name '*.md' -print0 | while IFS= read -r -d '' file; do
     pandoc "$file" -o "${file}.html"
 done
 
+# set docs as browser home page
+echo 'user_pref("browser.startup.homepage", "file:///home/vagrant/docker-hack-night/docs/index.html");' >> /home/vagrant/.mozilla/firefox/cw4qa3h5.default/prefs.js
